6445337ce991214fba4a42f3a9f85d737a377454
utamuratov <43562558+utamuratov@users.noreply.github.com>
2024-05-15 16:34:51 +0500

    ng-zorro-antd added and configure; informed about dom manipulation issue for hydration enabled project

--- modules/client/src/lib/client/client.component.ts
+++ modules/client/src/lib/client/client.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'lib-client',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NzButtonModule, NzIconModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css',
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import {
+  SearchOutline,
+  StepBackwardOutline,
+} from '@ant-design/icons-angular/icons';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    provideClientHydration(),
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    importProvidersFrom(
+      NzIconModule.forRoot([SearchOutline, StepBackwardOutline])
+    ),
+  ],
 };
